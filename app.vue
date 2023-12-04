a981035c0c73607ac5f9d05e576708850370eb96
Alan Lima <dev.alanlima@gmail.com>
2023-12-04 20:36:23 -0300

    first commit

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex flex-col gap-10 pt-10">
+    <div class="mx-auto w-fit">
+      <ToggleButton v-model="bgRed" onLabel="Primary" offLabel="Secondary" />
+    </div>
+    <div :class="['block w-32 h-32 mx-auto rounded-2xl outline outline-[3px] outline-[var(--surface-d)]', bgRed ? 'bg-[var(--surface-d)]' : 'bg-[var(--surface-e)]']"/>
+  </div>
+</template>
+
+<script setup>
+const bgRed = ref(true)
+</script>
