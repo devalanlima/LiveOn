c6f5800115bb48c7702e52202a9497379fc52f83
Alan Lima <dev.alanlima@gmail.com>
2023-12-08 13:14:54 -0300

    feat: Add new var colors to TailwindCss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-      "./components/**/*.{js,vue,ts}",
-      "./layouts/**/*.vue",
-      "./pages/**/*.vue",
-      "./plugins/**/*.{js,ts}",
-      "./app.vue",
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
   ],
   theme: {
+    extend: {
+      colors: {
+        surface_a: 'rgb(var(--surface-a-tw) / <alpha-value>)',
+        primary_color: 'rgb(var(--primary-color-tw) / <alpha-value>)',
+        primary_color_text: 'rgb(var(--primary-color-text-tw) / <alpha-value>)',
+        secondary_color_text: 'rgb(var(--secondary-color-text-tw) / <alpha-value>)',
+      }
+    }
   },
   plugins: [],
 }
