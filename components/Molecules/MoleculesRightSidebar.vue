c21d5875edf98e9193ff705ccd72a9750a02227c
Alan Lima <dev.alanlima@gmail.com>
2023-12-08 13:17:19 -0300

    feat: Create right sidebar component

---
+++ components/Molecules/MoleculesRightSidebar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div :class="[' flex max-w-[345px] min-w-[20px] relative overflow-hidden', isOpen ? 'w-full' : 'w-0']">
+    <button
+      class="w-[20px] h-full bg-primary_color relative overflow-hidden"
+      @click="isOpen = !isOpen"
+    >
+      <Icon
+        :name="isOpen ? 'prime:angle-right' : 'prime:angle-left'"
+        size="30"
+        class="-translate-x-1"
+      />
+
+    </button>
+    <div class="flex flex-col grow">
+      <AtomsTwitchPlayer v-if="isOpen" />
+      <AtomsTwitchChat v-if="isOpen" class="grow" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const isOpen = ref(false);
+</script>
+
+<style scoped></style>
