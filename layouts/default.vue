a81a920a1f272c1cdc654e95ffee7cb252302115
Alan Lima <dev.alanlima@gmail.com>
2023-12-08 13:16:31 -0300

    feat: Create default layout

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex w-full h-screen">
+    <div class="grow relative">
+      <header class="absolute w-full">
+        <PrimeVueNavbar class="h-full" />
+      </header>
+      <div class="flex h-screen pt-[68px]">
+        <MoleculesNews/>
+        <slot />
+      </div>
+    </div>
+    <MoleculesRightSidebar />
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
