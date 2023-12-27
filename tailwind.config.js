654aa082f4c53f893c15d3dbb46707faab6e10f5
Alan Lima <dev.alanlima@gmail.com>
2023-12-27 17:07:42 -0300

    feat: Add new secondary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ module.exports = {
       colors: {
         surface_a: 'rgb(var(--surface-a-tw) / <alpha-value>)',
         primary_color: 'rgb(var(--primary-color-tw) / <alpha-value>)',
+        secondary_color: 'rgb(var(--secondary-color-tw) / <alpha-value>)',
         primary_color_text: 'rgb(var(--primary-color-text-tw) / <alpha-value>)',
         secondary_color_text: 'rgb(var(--secondary-color-text-tw) / <alpha-value>)',
       }
