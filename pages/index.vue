be19692325c45f7310438925e4e6ccca70ddb75a
Alan Lima <dev.alanlima@gmail.com>
2023-12-27 17:17:16 -0300

    refactor: Change example API and add condition in PrimeVuePanel

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,16 @@
 <template>
-  <div class="h-full w-full overflow-hidden">
+  <div class="h-full w-full overflow-hidden relative">
     <VirtualScroller
       :items="data"
       :itemSize="672"
       class="w-full h-full px-5"
     >
       <template v-slot:item="{ item, options }">
-        <PrimeVuePanel :url-image="item.download_url"
-        :atual-post="options.index"
-        :total-posts="options.count"
+        <PrimeVuePanel
+          v-if="!pending"
+          :url-image="item.url"
+          :atual-post="options.index"
+          :total-posts="options.count"
         />
       </template>
     </VirtualScroller>
@@ -16,16 +18,17 @@
 </template>
 
 <script setup lang="ts">
+
 const { data, pending } = await useFetch<[
   {
     "id": string;
-    "author": string;
-    "height": number;
-    "width": number;
     "url": string;
-    "download_url": string;
-  }]
->('https://picsum.photos/v2/list?page=1&limit=100');
+    "width": number; 
+    "height": number;
+    "breeds": Array<string>;
+    "favourite": object;
+  }
+]>('https://api.thecatapi.com/v1/images/search?limit=100&api_key=live_oycSzK0NXRk1YswSVXgADhhfsLLz32Ic3vDwO5t3H7CGUQPXTYmB9ahtz3yWomdW')
 
 </script>
 
