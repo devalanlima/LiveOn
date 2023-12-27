4d9cdd08aea35d82c0c391e49ea3ded8f2e98045
Alan Lima <dev.alanlima@gmail.com>
2023-12-27 17:15:17 -0300

    refactor: Change from moleculesNews to PrimeVueNavbar and change layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,8 @@
 <template>
   <div class="flex w-full h-screen">
     <div class="grow relative">
-      <header class="absolute w-full">
-        <PrimeVueNavbar class="h-full" />
-      </header>
-      <div class="flex h-screen pt-[68px]">
-        <MoleculesNews/>
+      <div class="flex h-screen">
+        <PrimeVueNavbar/>
         <slot />
       </div>
     </div>
