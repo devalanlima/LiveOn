ea2390141b92517a6952c4171225fd3d3db6c70d
Alan Lima <dev.alanlima@gmail.com>
2023-12-28 10:21:20 -0300

    feat: Add title to post design

--- components/PrimeVue/PrimeVuePanel.vue
+++ components/PrimeVue/PrimeVuePanel.vue
@@ -1,11 +1,15 @@
 <template>
   <section
-    class="grid grid-rows-[50px_500px_50px] grid-cols-1 outline outline-2 outline-primary_color rounded-xl justify-between w-full max-w-[900px] py-2 gap-2 overflow-hidden mt-10 mx-auto"
+    class="prime-vue-panel grid grid-rows-[50px_500px_50px] grid-cols-1 outline outline-1 outline-primary_color rounded-xl justify-between w-full max-w-[900px] py-2 gap-2 overflow-hidden mt-10 mx-auto animation-duration-1000 animation-ease-in-out"
   >
-    <header class="row-start-1 row-end-2 px-3 flex justify-between h-[40px]">
+    <header class="row-start-1 row-end-2 px-3 flex justify-between items-center h-full">
       <div class="flex gap-2 items-center">
-        <Avatar />
-        <p>Esquilocalvodameianoite</p>
+        <Icon
+          name="prime:user"
+          size="40"
+          class="rounded-full bg-orange-500 text-black p-1"
+        />
+        <p class="text-lg font-semibold">Esquilocalvodameianoite</p>
       </div>
       <div class="flex gap-2 items-center">
         <Icon
@@ -17,12 +21,14 @@
         </p>
       </div>
     </header>
-    <main class="row-start-2 row-end-3 bg-primary_color p-3 overflow-hidden">
-      <div class="w-full h-full grid place-items-center rounded-xl overflow-hidden">
-        <Image
-          :src="props.urlImage"
-          alt="Image"
-          preview
+    <main
+      class="row-start-2 row-end-3 flex items-center justify-center outline outline-1 outline-primary_color p-3 panel-image"
+    >
+      <div class="h-full gap-4 grid grid-rows-[auto_1fr]">
+        <h2 class="font-semibold text-2xl">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
+        <PrimeVueImage
+          class="min-h-[300px]"
+          :url-image="props.urlImage"
         />
       </div>
     </main>
@@ -78,27 +84,22 @@ interface Props {
 
 const props = defineProps<Props>()
 
-
 </script>
 
 <style>
-.p-toolbar {
+.prime-vue-panel .p-toolbar {
   @apply h-full p-0 pb-0 m-0 mt-0 border-none px-3 !important;
 }
 
-.p-toolbar-group-start {
+.prime-vue-panel .p-toolbar-group-start {
   @apply flex gap-2;
 }
 
-.p-toolbar-group-center {
+.prime-vue-panel .p-toolbar-group-center {
   @apply flex gap-2;
 }
 
 .p-tooltip-text {
-  @apply text-primary_color_text !important
-}
-
-.p-image-toolbar {
-  z-index: 9999999;
+  @apply text-secondary_color_text !important
 }
 </style>
